3f4634a4ea0b1e7ba22b5b9b78e0ecd4e17a987b
Buie Blakeley <bblakeley@clickherelabs.com>
2014-03-18 09:36:36 -0500

    Started #4

---
+++ 4.py
@@ -0,0 +1,12 @@
+# Problem 4
+# http://projecteuler.net/problem=4
+
+print "Searching for largest palindrome made from the product of two 3-digit numbers..."
+
+# initialize two 3-digit number variables
+# make them both equal to 999
+# store the product of the two numbers in another variable
+# check if that number is a palindrome
+# if a palindrome, check to see if it is larger than the previous max palindrome
+# if not a palindrome, increment down one of the three digit numbers
+
